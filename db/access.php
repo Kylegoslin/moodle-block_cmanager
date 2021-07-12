bd3af83265579570ce061a9d43aff2b747c18db3
nstefanski <nmstefanski@gmail.com>
2021-07-12 15:37:02 -0500

    Update access.php
    removed duplicate line

--- db/access.php
+++ db/access.php
@@ -31,7 +31,6 @@ $capabilities = array(
         'contextlevel' => CONTEXT_SYSTEM,
         'archetypes' => array(
             'editingteacher' => CAP_ALLOW,
-            'editingteacher' => CAP_ALLOW,
             'coursecreator' => CAP_ALLOW,
             'manager' => CAP_ALLOW
             
@@ -46,7 +45,6 @@ $capabilities = array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_BLOCK,
         'archetypes' => array(
-            'editingteacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
             'coursecreator' => CAP_ALLOW,
             'manager' => CAP_ALLOW
